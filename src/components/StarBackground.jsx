0f5d17f436b3a041f5454ed7ab5e8408bf86d9aa
Mohiuddin10 <sj.ctg6737@gmail.com>
2025-07-03 00:35:54 +0600

    meteors added

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react";
 
 // start has : id, x, y, size, opacity, animationDuration
 
+// meteor has : id, x, y, size, delay, animationDuration
+
 export const StarBackground = () => {
   // set star in useState
   const [stars, setStars] = useState([]);
+  const [meteors, setMeteors] = useState([]);
 
   useEffect(() => {
     generateStars();
+    generateMeteors();
   }, []);
 
   // Generate stars
@@ -35,6 +39,28 @@ export const StarBackground = () => {
 
     setStars(newStars);
   };
+  // meteors
+  const generateMeteors = () => {
+    // 1. Calculate number of meteors
+    const numberOfMeteors = 3;
+
+    // 2. Create an array of meteors
+    const newMeteors = [];
+
+    // 3. Generate random stars
+    for (let i = 0; i < numberOfMeteors; i++) {
+      // 4. push meteors to newMeteors obj
+      newMeteors.push({
+        id: i,
+        x: Math.random() * 100,
+        y: Math.random() * 20,
+        size: Math.random() * 2 + 1,
+        animationDelay: Math.random() * 10,
+        animationDuration: Math.random() * 5 + 3,
+      });
+    }
+    setMeteors(newMeteors);
+  };
   return (
     <div className="fixed inset-0 overflow-hidden pointer-events-none z-0">
       {stars.map((star) => (
@@ -48,6 +74,20 @@ export const StarBackground = () => {
             height: `${star.size}px`,
             opacity: star.opacity,
             animationDuration: `${star.animationDuration}s`,
+          }}
+        />
+      ))}
+      {meteors.map((meteor) => (
+        <div
+          key={meteor.id}
+          className="meteor animate-meteor"
+          style={{
+            left: `${meteor.x}%`,
+            top: `${meteor.y}%`,
+            width: `${meteor.size * 20}px`,
+            height: `${meteor.size}px`,
+            animationDelay: `${meteor.delay}s`,
+            animationDuration: `${meteor.animationDuration}s`,
           }}
         />
       ))}
